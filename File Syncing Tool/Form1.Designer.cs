825e2a3609381ab17091fab660eb461f0422d475
zhiyan114 <Private@NotRevealing.It>
2018-12-30 20:46:50 -0500

    Minor UI Modification, Output logs will now display timestamp for each logs output, Added Clear and Export the logs option, and finally, finished with configuration file (ability to save and load textbox values).

--- File Syncing Tool/Form1.Designer.cs
+++ File Syncing Tool/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -47,6 +46,12 @@
             this.button5 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // notifyIcon1
@@ -82,17 +87,6 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Free and Open Source File Syncing Tool by zhiyan114";
             // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(9, 263);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(63, 13);
-            this.linkLabel1.TabIndex = 3;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Visit Source";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(583, 60);
@@ -101,6 +95,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "Select A Folder";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
@@ -126,11 +121,12 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "Select A Folder";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 141);
+            this.label4.Location = new System.Drawing.Point(12, 126);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(150, 13);
             this.label4.TabIndex = 8;
@@ -138,34 +134,36 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(168, 138);
+            this.textBox3.Location = new System.Drawing.Point(168, 123);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(184, 20);
             this.textBox3.TabIndex = 9;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 168);
+            this.button3.Location = new System.Drawing.Point(15, 149);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.Size = new System.Drawing.Size(119, 23);
             this.button3.TabIndex = 10;
             this.button3.Text = "Start Syncing";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 197);
+            this.button4.Location = new System.Drawing.Point(15, 178);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(92, 23);
+            this.button4.Size = new System.Drawing.Size(119, 23);
             this.button4.TabIndex = 11;
             this.button4.Text = "Stop Syncing";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(12, 226);
+            this.button5.Location = new System.Drawing.Point(15, 207);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(122, 23);
+            this.button5.Size = new System.Drawing.Size(119, 23);
             this.button5.TabIndex = 12;
             this.button5.Text = "Hide to System Tray";
             this.button5.UseVisualStyleBackColor = true;
@@ -187,14 +185,80 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
             this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox4.Size = new System.Drawing.Size(304, 131);
+            this.textBox4.Size = new System.Drawing.Size(304, 146);
             this.textBox4.TabIndex = 14;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(168, 149);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(184, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Load Data from Config File";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(168, 178);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(184, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Save Data to Config File";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(168, 207);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(184, 23);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "Delete Config File";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(15, 236);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(119, 23);
+            this.button9.TabIndex = 18;
+            this.button9.Text = "Clear Console";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(168, 236);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(184, 23);
+            this.button10.TabIndex = 19;
+            this.button10.Text = "Export Console Logs";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(15, 265);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(337, 23);
+            this.button11.TabIndex = 20;
+            this.button11.Text = "View Source Code On Github";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(685, 285);
+            this.ClientSize = new System.Drawing.Size(685, 293);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button5);
@@ -206,13 +270,13 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "File Update";
+            this.Text = "File Syncing Tool";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -225,7 +289,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
@@ -238,6 +301,12 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
     }
 }
 
--- File Syncing Tool/Form1.cs
+++ File Syncing Tool/Form1.cs
@@ -4,32 +4,46 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace File_Syncing_Tool
 {
     public partial class Form1 : Form
     {
+        string ConfigFileName = @"Config.xml"; // Default Configuration File Name
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            Process.Start("https://github.com/zhiyan114/File-Syncing-Tool/"); // Take the user to the repository
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Not sure what this can do anything yet xD
+            WriteConsole("Software Loaded...");
+            if (!File.Exists(ConfigFileName))
+            {
+                // Create the XML Configuration File and the required attribute from Scratch and also shows a messagebox which assume the user is new to this software as the configuration file isn't existing
+                XmlDocument config = new XmlDocument();
+                XmlElement TheHeadXML = config.CreateElement("FileSyncingTool");
+                config.AppendChild(TheHeadXML);
+                XmlAttribute CreditData = config.CreateAttribute("Credits");
+                CreditData.Value = "A Software developed by zhiyan114";
+                TheHeadXML.Attributes.Append(config.CreateAttribute("Credit"));
+                TheHeadXML.Attributes.Append(CreditData);
+                TheHeadXML.Attributes.Append(config.CreateAttribute("OpenLocation"));
+                TheHeadXML.Attributes.Append(config.CreateAttribute("SaveLocation"));
+                TheHeadXML.Attributes.Append(config.CreateAttribute("TimePerSave"));
+                config.Save(ConfigFileName);
+                MessageBox.Show("Thank You for using File Syncing Tool for the first time. Software developed by zhiyan114. This software will allows automatically backup all the files from a folder to another location. This software is also open source so you can check it out. If you have any issues, you may report the issues.","First Time",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
         }
         private void WriteConsole(string message)
         {
-            textBox4.Text = textBox4.Text + message + "\r\n"; // Proper way to write data to a TextBox alike a console method
+            textBox4.Text = textBox4.Text + DateTime.Now.ToString("MM/dd/yyyy h:mm:ss tt") + ": " + message + "\r\n"; // Proper way to write data to a TextBox alike a console method
         }
         private void ClearConsole()
         {
@@ -47,6 +61,143 @@ namespace File_Syncing_Tool
         {
             Show();
             notifyIcon1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure to delete the configuration file? Once it deleted, it cannot be recovered and the software will automatically shutdown.","Confirmation",MessageBoxButtons.YesNo,MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                if(File.Exists(ConfigFileName))
+                {
+                    File.Delete(ConfigFileName);
+                    MessageBox.Show("The configuration File is now deleted. Press OK and the software will automatically shutdown.","Config Deleted",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                } else
+                {
+                    MessageBox.Show("The Configuration File does not exist, please ensure that it not deleted while the software is running. If it is please restart the software immediately. Press OK and the software will shutdown.","Config File Not Existed",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+                Close(); // You need the configuration file in order for the software to fully function. If it not existed, software require shutdown to prevent unhandled/undiscovered exception
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // Check if the logs is empty so that user doesn't export nothing (which creates a useless file)
+            if(string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                MessageBox.Show("You cannot export the console logs since it already empty", "Empty Logs", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            using (SaveFileDialog SaveLogLocation = new SaveFileDialog())
+            {
+                if(SaveLogLocation.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.WriteAllText(SaveLogLocation.FileName, textBox4.Text);
+                        MessageBox.Show("The Console logs has been successfully saved under this path:\n"+SaveLogLocation.ToString(),"Logs Saved",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    } catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message,"Error While Saving Logs",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    }
+                } else
+                {
+                    MessageBox.Show("There was an error while selecting a save location OR You probably canceled it.","Unable to Save Log",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Check if the logs is empty so that user doesn't clear nothing
+            if (string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                MessageBox.Show("You cannot clear the console since it already empty","Empty Logs",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+                return;
+            }
+            DialogResult ClearConsoleResult = MessageBox.Show("Would you like to display a log that shows you cleared the console? \n\n Yes - Clear with Log \n\n No - Clear without Log \n\n Cancel - Don't clear the Log", "Clearing Logs", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            if (ClearConsoleResult == DialogResult.Yes)
+            {
+                ClearConsole();
+                WriteConsole("Console Logs was Cleared");
+                MessageBox.Show("All the logs on the console has now been cleared with a log.","Console Cleared",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            } else if(ClearConsoleResult == DialogResult.No)
+            {
+                ClearConsole();
+                MessageBox.Show("All the logs on the console has now been cleared without a log.", "Console Cleared", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/zhiyan114/File-Syncing-Tool/"); // Take the user to the repository
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // This will get each XML Node and Save the necessary data into it
+            XmlDocument Config = new XmlDocument();
+            Config.Load(ConfigFileName);
+            foreach(XmlNode MultiObj in Config.SelectNodes("FileSyncingTool"))
+            {
+                // Define each XML Nodes
+                XmlNode OpenFileLocation = MultiObj.SelectSingleNode("OpenLocation");
+                XmlNode SaveFileLocation = MultiObj.SelectSingleNode("SaveLocation");
+                XmlNode TimePerSave = MultiObj.SelectSingleNode("TimePerSave");
+                // Saves the data into the XML and notify the user that it has been saved
+                OpenFileLocation.Value = textBox1.Text;
+                SaveFileLocation.Value = textBox2.Text;
+                TimePerSave.Value = textBox3.Text;
+                Config.Save(ConfigFileName);
+                MessageBox.Show("All the configuration has been saved","Config Saved",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Does almost same thing as saving the data
+            XmlDocument Config = new XmlDocument();
+            Config.Load(ConfigFileName);
+            foreach (XmlNode MultiObj in Config.SelectNodes("FileSyncingTool"))
+            {
+                // Define each XML Nodes
+                XmlNode OpenFileLocation = MultiObj.SelectSingleNode("OpenLocation");
+                XmlNode SaveFileLocation = MultiObj.SelectSingleNode("SaveLocation");
+                XmlNode TimePerSave = MultiObj.SelectSingleNode("TimePerSave");
+                // Perform a check to see if the XML Node does not exist
+                if(OpenFileLocation != null && SaveFileLocation != null && TimePerSave != null)
+                {
+                    // Load the configuration file and tell the user that it was loaded
+                    textBox1.Text = OpenFileLocation.Value;
+                    textBox2.Text = SaveFileLocation.Value;
+                    textBox3.Text = TimePerSave.Value;
+                    MessageBox.Show("All the configurations has been successfully loaded.", "Config Loaded", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else
+                {
+                    // Tell the user that the required XML Node is null
+                    MessageBox.Show("Sorry but the configuration file are missing datas, please try again by deleting the configuration file and restart the software.","Missing Configuration",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+
         }
     }
 }
